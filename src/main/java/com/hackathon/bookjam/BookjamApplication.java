fa228ec13ffcffea119c200dab53508e33225b88
tteia <tteia.dl@gmail.com>
2024-07-31 14:21:04 +0900

    해커톤 스프링 환경 세팅

---
+++ src/main/java/com/hackathon/bookjam/BookjamApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bookjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookjamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookjamApplication.class, args);
+	}
+
+}
